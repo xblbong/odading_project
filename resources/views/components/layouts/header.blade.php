9fee94860f8949abf8aa507ad5ec9038cdbfcba0
xblbong <sblhh.m@gmail.com>
2025-10-25 15:15:17 +0700

    feat(header.blade.php): add new feature

---
+++ resources/views/components/layouts/header.blade.php
@@ -0,0 +1,45 @@
+<header class="bg-[#ff8c00]">
+    <nav class="container flex items-center justify-between w-full">
+        <div class="flex items-center gap-x-3 p-5">
+            {{-- <img src="O" alt="logo" class="h-10 w-10 bg-white opacity-70 blur-[2px] rounded-full text-center" /> --}}
+            <span
+                class="h-10 w-10 flex items-center blur-[0.2px] justify-center bg-white/30 opacity-70 rounded-full font-bold text-xl">
+                <h4 class="text-white">R</h4>
+            </span>
+            <span class="font-bold text-white text-2xl hover:text-shadow-[0_0_5px_rgba(255,255,255,0.8)]">Odading</span>
+        </div>
+        <ul class="flex items-center gap-x-10 font-semibold text-white/90 text-lg mr-20 ">
+            <li class="flex items-center flex-col group">
+                <a href="#home"
+                    class="hover:text-white hover:text-shadow-[0_0_5px_rgba(255,255,255,0.8)] transition duration-300">Home</a>
+                <span
+                    class="bg-white/80 w-0 h-1 rounded-full transition-all duration-300 group-hover:w-[60%] group-hover:bg-white group-hover:shadow-[0_0_10px_rgba(255,255,255,0.8)]"></span>
+            </li>
+            <li class="flex items-center flex-col group">
+                <a href="#tentang-kami"
+                    class="hover:text-white hover:text-shadow-[0_0_5px_rgba(255,255,255,0.8)] transition duration-300">Tentang
+                    Kami</a>
+                <span
+                    class="bg-white/80 w-0 h-1 rounded-full transition-all duration-300 group-hover:w-[60%] group-hover:bg-white group-hover:shadow-[0_0_10px_rgba(255,255,255,0.8)]"></span>
+            </li>
+            <li class="flex items-center flex-col group">
+                <a href="#menu"
+                    class="hover:text-white hover:text-shadow-[0_0_5px_rgba(255,255,255,0.8)] transition duration-300">Menu</a>
+                <span
+                    class="bg-white/80 w-0 h-1 rounded-full transition-all duration-300 group-hover:w-[60%] group-hover:bg-white group-hover:shadow-[0_0_10px_rgba(255,255,255,0.8)]"></span>
+            </li>
+            <li class="flex items-center flex-col group">
+                <a href="#lokasi"
+                    class="hover:text-white hover:text-shadow-[0_0_5px_rgba(255,255,255,0.8)] transition duration-300">Lokasi</a>
+                <span
+                    class="bg-white/80 w-0 h-1 rounded-full transition-all duration-300 group-hover:w-[60%] group-hover:bg-white group-hover:shadow-[0_0_10px_rgba(255,255,255,0.8)]"></span>
+            </li>
+            <li class="flex items-center flex-col group">
+                <a href="#kontak"
+                    class="hover:text-white hover:text-shadow-[0_0_5px_rgba(255,255,255,0.8)] transition duration-300">Kontak</a>
+                <span
+                    class="bg-white/80 w-0 h-1 rounded-full transition-all duration-300 group-hover:w-[60%] group-hover:bg-white group-hover:shadow-[0_0_10px_rgba(255,255,255,0.8)]"></span>
+            </li>
+        </ul>
+    </nav>
+</header>
